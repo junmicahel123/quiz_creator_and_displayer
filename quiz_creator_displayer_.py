b62540023656215683b9031a85811110724e9b9b
Owen <juanmig134187@gmail.com>
2025-05-27 14:59:08 +0800

    pseudocoding_and_importing

---
+++ quiz_creator_displayer_.py
@@ -0,0 +1,12 @@
+# 
+# import tkinter
+    # import quiz creator
+    # import quiz displayer
+#initialize Class 
+    # def initialization of roots for gui
+    # def for quiz creator
+    # def for quiz displayer
+
+import tkinter as tk
+
+
